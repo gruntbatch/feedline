ffce90083d15e3c75eef7157250db920094ace15
gruntbatch <gruntbatch@gmail.com>
2023-03-09 11:12:36 -0800

    Package page handlers together

--- internal/server/page/page.go
+++ internal/server/page/page.go
@@ -1,7 +1,76 @@
 package page
 
-import "net/http"
+import (
+	"feedline/internal/message"
+	"feedline/pkg/receiver"
+	"net/http"
+	"path"
+	"text/template"
+)
+
+var WebDir string
 
 func Index(w http.ResponseWriter, r *http.Request) {
+	url := r.URL.Query().Get("url")
+	var bulletins []receiver.Bulletin
+	if url == "" {
+		bulletins = receiver.UnreadBulletins()
+	} else {
+		var err error
+		bulletins, err = receiver.AllBulletinsFromSanitizedUrl(url)
+		if err != nil {
+			panic(err)
+		}
+	}
 
+	var t *template.Template
+	if len(bulletins) != 0 {
+		t = template.Must(template.ParseFiles(
+			path.Join(WebDir, "base.html"),
+			path.Join(WebDir, "feed.html"),
+		))
+		if err := t.Execute(w, bulletins); err != nil {
+			panic(err)
+		}
+	} else {
+		t = template.Must(template.ParseFiles(
+			path.Join(WebDir, "base.html"),
+			path.Join(WebDir, "message.html"),
+		))
+		if err := t.Execute(w, message.EmptyFeed()); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func Settings(w http.ResponseWriter, r *http.Request) {
+	var t *template.Template
+	t = template.Must(template.ParseFiles(
+		path.Join(WebDir, "base.html"),
+		path.Join(WebDir, "settings.html")))
+	if err := t.Execute(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+func Subscriptions(w http.ResponseWriter, r *http.Request) {
+	channels := receiver.AllChannels()
+
+	var t *template.Template
+	if len(channels) != 0 {
+		t = template.Must(template.ParseFiles(
+			path.Join(WebDir, "base.html"),
+			path.Join(WebDir, "channels.html")))
+		if err := t.Execute(w, channels); err != nil {
+			panic(err)
+		}
+	} else {
+		t = template.Must(template.ParseFiles(
+			path.Join(WebDir, "base.html"),
+			path.Join(WebDir, "message.html"),
+		))
+		if err := t.Execute(w, message.NoChannels()); err != nil {
+			panic(err)
+		}
+	}
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,92 +1,25 @@
 package server
 
 import (
-	"feedline/internal/message"
 	"feedline/internal/server/api"
-	"feedline/pkg/receiver"
-	"html/template"
+	"feedline/internal/server/page"
 	"log"
 	"net/http"
-	"path"
 )
 
 var webDir string
 
-func Serve(_webDir string, addr string) {
-	webDir = _webDir
-	http.HandleFunc("/", handleIndex)
-	http.HandleFunc("/settings/", handleSettings)
-	http.HandleFunc("/subscriptions/", handleSubscriptions)
+func Serve(webDir string, addr string) {
+	page.WebDir = webDir
 
 	http.HandleFunc("/api/dismiss/", api.Dismiss)
 	http.HandleFunc("/api/feed/", api.Feed)
 	http.HandleFunc("/api/refresh/", api.Refresh)
 	http.HandleFunc("/api/tidy/", api.Tidy)
 
+	http.HandleFunc("/", page.Index)
+	http.HandleFunc("/settings/", page.Settings)
+	http.HandleFunc("/subscriptions/", page.Subscriptions)
+
 	log.Fatal(http.ListenAndServe(addr, nil))
-}
-
-func handleIndex(w http.ResponseWriter, r *http.Request) {
-	url := r.URL.Query().Get("url")
-	var bulletins []receiver.Bulletin
-	if url == "" {
-		bulletins = receiver.UnreadBulletins()
-	} else {
-		var err error
-		bulletins, err = receiver.AllBulletinsFromSanitizedUrl(url)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	var t *template.Template
-	if len(bulletins) != 0 {
-		t = template.Must(template.ParseFiles(
-			path.Join(webDir, "base.html"),
-			path.Join(webDir, "feed.html"),
-		))
-		if err := t.Execute(w, bulletins); err != nil {
-			panic(err)
-		}
-	} else {
-		t = template.Must(template.ParseFiles(
-			path.Join(webDir, "base.html"),
-			path.Join(webDir, "message.html"),
-		))
-		if err := t.Execute(w, message.EmptyFeed()); err != nil {
-			panic(err)
-		}
-	}
-}
-
-func handleSettings(w http.ResponseWriter, r *http.Request) {
-	var t *template.Template
-	t = template.Must(template.ParseFiles(
-		path.Join(webDir, "base.html"),
-		path.Join(webDir, "settings.html")))
-	if err := t.Execute(w, nil); err != nil {
-		panic(err)
-	}
-}
-
-func handleSubscriptions(w http.ResponseWriter, r *http.Request) {
-	channels := receiver.AllChannels()
-
-	var t *template.Template
-	if len(channels) != 0 {
-		t = template.Must(template.ParseFiles(
-			path.Join(webDir, "base.html"),
-			path.Join(webDir, "channels.html")))
-		if err := t.Execute(w, channels); err != nil {
-			panic(err)
-		}
-	} else {
-		t = template.Must(template.ParseFiles(
-			path.Join(webDir, "base.html"),
-			path.Join(webDir, "message.html"),
-		))
-		if err := t.Execute(w, message.NoChannels()); err != nil {
-			panic(err)
-		}
-	}
 }
