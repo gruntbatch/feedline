588df0cb68a3160e47dbfe669a504509f1effb98
gruntbatch <gruntbatch@gmail.com>
2024-01-29 11:20:59 -0800

    Configure refresh interval via command line

--- cmd/feedline/main.go
+++ cmd/feedline/main.go
@@ -4,16 +4,18 @@ import (
 	"feedline/internal/server"
 	"feedline/pkg/receiver"
 	"flag"
+	"time"
 )
 
 func main() {
 	webDir := flag.String("webdir", "./web", "load templates from this directory")
 	addr := flag.String("addr", ":8080", "listen to this address")
+	interval := flag.Duration("interval", time.Duration(10 * float64(time.Minute)), "time between refreshes")
 	flag.Parse()
 
 	go func() {
 		receiver.Refresh()
 	}()
-	go receiver.Listen()
+	go receiver.Listen(*interval)
 	server.Serve(*webDir, *addr)
 }
--- pkg/receiver/receiver.go
+++ pkg/receiver/receiver.go
@@ -12,13 +12,13 @@ func Refresh() {
 	}
 }
 
-func Listen() {
+func Listen(interval time.Duration) {
 	// Currently, this doesn't do anything to shut itself down cleanly,
 	// however, I'm not sure it needs to. As long as Refresh() doesn't perform
 	// any disk operations, I'm not sure there's anything to clean up.
 
 	// TODO Update at a user-configured time interval
-	ticker := time.NewTicker(10 * time.Minute)
+	ticker := time.NewTicker(interval)
 	for {
 		select {
 		case <-ticker.C:
